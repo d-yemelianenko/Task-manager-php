ed04437d0d8ced66bfb10aac60bb945453315e4c
Daria <dashaemelianenko235@gmail.com>
2025-03-26 13:14:09 +0100

    changing the look of the index page

--- index.php
+++ index.php
@@ -12,6 +12,44 @@ include APP_ROOT . '/includes/header.php';
 <section>
     <article>
         <?php if (!empty($tasks)) : ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Content</th>
+                        <th>Data stworzenia</th>
+                        <th>Data modyfikacji</th>
+                        <th>Data wykonania</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php foreach ($tasks as $task): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($task['title']); ?></td>
+                            <td><?= htmlspecialchars($task['description']); ?></td>
+                            <td><?= htmlspecialchars($task['created_at']); ?></td>
+                            <td><?= htmlspecialchars($task['updated_at']); ?></td>
+                            <td><?= htmlspecialchars($task['due_date']); ?></td>
+                            <td><a href="edit.php?id=<?= htmlspecialchars($task['id']) ?>" class="btn-edit">Edytuj</a></td>
+                            <td><a href="delete.php?id=<?= htmlspecialchars($task['id']) ?>" class="btn-delete">Usuń</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="4">Brak postów do wyświetlenia.</td>
+                    </tr>
+
+                <?php endif; ?>
+                </tbody>
+            </table>
+    </article>
+
+
+
+    
+    <article>
+        <?php if (!empty($tasks)) : ?>
             <?php foreach ($tasks as $task): ?>
                 <h2><?= htmlspecialchars($task['title']) ?></h2>
                 <p><?= nl2br(htmlspecialchars($task['description'])) ?></p>
